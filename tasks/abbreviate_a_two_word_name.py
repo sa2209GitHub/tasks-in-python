3bbbd73e741eabb3b6a9e68fe2ab087aa24710ad
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-10-25 19:17:59 +0300

    Abbreviate a Two Word Name

--- tasks/abbreviate_a_two_word_name.py
+++ tasks/abbreviate_a_two_word_name.py
@@ -12,14 +12,18 @@ name_4 = 'P Favuzzi'        # 'P.F'
 name_5 = 'David Mendieta'   # 'D.M'
 name_6 = 'elon musk'        # 'E.M'
 
-# Solution
+# Second solution
 def abbrev_name(name):
     name = name.split(' ')
-    
+
     first_abbr = name[0][0].upper()
     second_abbr = name[1][0].upper()
 
     return f'{first_abbr}.{second_abbr}'
+
+# Second solution
+def abbrev_name_2(name):
+    return name.split()[0][0].upper() + '.' + name.split()[1][0].upper()
 
 # Output
 print(abbrev_name(name_1))
@@ -29,3 +33,9 @@ print(abbrev_name(name_4))
 print(abbrev_name(name_5))
 print(abbrev_name(name_6))
 
+print(abbrev_name_2(name_1))
+print(abbrev_name_2(name_2))
+print(abbrev_name_2(name_3))
+print(abbrev_name_2(name_4))
+print(abbrev_name_2(name_5))
+print(abbrev_name_2(name_6))
