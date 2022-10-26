cd50d196e05cb6738b765b8eafc978b977f46b7c
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-10-26 21:00:04 +0300

    Well of Ideas (Harder Version)

--- tasks/well_of_ideas_harder_version.py
+++ tasks/well_of_ideas_harder_version.py
@@ -4,41 +4,24 @@
 '''
 
 # Variables
-from operator import is_
-
-
-ideas_1 = ['bad', 'bad', 'bad']                                                 # 'Fail!'
-ideas_2 = ['good', 'bad', 'bad', 'bad', 'bad']                                  # 'Publish!'
-ideas_3 = ['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']    # 'I smell a series!'
+ideas_1 = [['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad']]                         # 'Fail!'
+ideas_2 = [['gOOd', 'bad', 'BAD', 'bad', 'bad'], ['bad', 'bAd', 'bad'], ['GOOD', 'bad', 'bad', 'bAd']]  # 'Publish!'
+ideas_3 = [['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']]                      # 'I smell a series!'
 
 # First solution
 def is_well_idea(ideas):
-    if 'good' not in ideas:
-        return 'Fail!'
-    else:
-        good_ideas_counter = 0
+    good_count = 0
 
-        for idea in ideas:
-            if idea == 'good':
-                good_ideas_counter += 1
-    
-    if good_ideas_counter > 2:
-        return 'I smell a series!'
+    for list in ideas:
+        for idea in list:
+            if idea.lower() == 'good': good_count += 1
 
-    return 'Publish!'
+    if good_count == 0: return 'Fail!'
+    if good_count > 2: return 'I smell a series!'
 
-# Second solution
-def is_well_idea_2(ideas):
-    if ideas.count('good') == 0:  return 'Fail!'
-    if ideas.count('good') > 2:   return 'I smell a series!'
     return 'Publish!'
 
 # Output
 print(is_well_idea(ideas_1))
 print(is_well_idea(ideas_2))
 print(is_well_idea(ideas_3))
-
-print(is_well_idea_2(ideas_1))
-print(is_well_idea_2(ideas_2))
-print(is_well_idea_2(ideas_3))
-
