7bee96e83001cf792c7e97b28b2218c47e80c875
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-10-26 21:18:47 +0300

    Well of Ideas (Harder Version)

--- tasks/well_of_ideas_harder_version.py
+++ tasks/well_of_ideas_harder_version.py
@@ -14,14 +14,23 @@ def is_well_idea(ideas):
 
     for list in ideas:
         for idea in list:
-            if idea.lower() == 'good': good_count += 1
+            if str(idea.lower()) == 'good': good_count += 1
 
     if good_count == 0: return 'Fail!'
     if good_count > 2: return 'I smell a series!'
 
     return 'Publish!'
 
+# Second solution
+def is_well_idea_2(ideas):
+    good_ideas = str(ideas).lower().count('good')
+    return 'I smell a series!' if good_ideas > 2 else 'Fail!' if not good_ideas else 'Publish!'
+
 # Output
 print(is_well_idea(ideas_1))
 print(is_well_idea(ideas_2))
 print(is_well_idea(ideas_3))
+
+print(is_well_idea_2(ideas_1))
+print(is_well_idea_2(ideas_2))
+print(is_well_idea_2(ideas_3))
